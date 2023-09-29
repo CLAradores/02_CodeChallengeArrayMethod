09a9721badb7060a8a2696bd3884fe62a3b5c9c7
charles1stgithub <charleslester.official@gmail.com>
2023-09-29 09:30:34 +0800

    Solution for Code Challenge

---
+++ script.js
@@ -0,0 +1,64 @@
+// Charllenge1
+const people = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@gmail.com",
+    phone: "111-111-1111",
+    age: 30,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Poe",
+    email: "jane@gmail.com",
+    phone: "222-222-2222",
+    age: 25,
+  },
+  {
+    firstName: "Bob",
+    lastName: "Foe",
+    email: "bob@gmail.com",
+    phone: "333-333-3333",
+    age: 45,
+  },
+  {
+    firstName: "Sara",
+    lastName: "Soe",
+    email: "Sara@gmail.com",
+    phone: "444-444-4444",
+    age: 19,
+  },
+  {
+    firstName: "Jose",
+    lastName: "Koe",
+    email: "jose@gmail.com",
+    phone: "555-555-5555",
+    age: 23,
+  },
+];
+
+const youngPeople = people.filter(
+  (person) =>
+    `${person.firstName} ${person.lastName} ${person.email}` && person.age <= 25
+);
+console.log(youngPeople);
+
+// Challenge 2 //////////////////////////////////////////////////////////////////////////////
+const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = numbers
+  .filter((posNum) => posNum > 0)
+  .reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+
+console.log(positiveSum);
+
+// Challenge 3 //////////////////////////////////////////////////////////////////////////////
+const words = ["coder", "programmer", "developer"];
+
+words.forEach((word) => {
+  console.log(
+    word.toString().charAt(0).toUpperCase() + word.slice(1, word.length)
+  );
+});
